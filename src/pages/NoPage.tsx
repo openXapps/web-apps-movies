6d7913554567b3f3188a4fae818d50f761d63de7
Gavin <gavin.dalton1@gmail.com>
2024-09-23 13:40:31 +0200

    Added error boundry

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,5 +1,10 @@
+import { useRouteError } from 'react-router-dom';
 
 export default function NoPage() {
+  const rrError = useRouteError();
+
+  console.log(rrError);
+
   return (
     <div>NoPage</div>
   );
