8b9a6334530ca4ebb1f36a5c731e3a9a0d3b4068
openXapps <32998735+openXapps@users.noreply.github.com>
2024-10-28 09:42:22 +0200

    Img slider fixes

--- src/components/MovieBackdrop.tsx
+++ src/components/MovieBackdrop.tsx
@@ -1,23 +1,24 @@
 import { useEffect, useState } from 'react';
-import { twMerge } from 'tailwind-merge';
 
-import { Button } from './ui/button';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 import { getMovie } from "@/lib/api";
 import { TmdbMovieImage, TmdbMovieImageData } from '@/lib/types';
 import ghostBackdrop from '@/assets/ghost-backdrop.jpg';
 
 export default function MovieBackdrop({ movieId, fallBack }: { movieId: string, fallBack: string }) {
-  // const [movieBackdrop, setMovieBackdrop] = useState<TmdbMovieImage[]>(initMovieImage);
   const [movieBackdrop, setMovieBackdrop] = useState<TmdbMovieImage[]>([]);
   const [currentImage, setCurrentImage] = useState(0);
-  const totalImages = movieBackdrop.length;
+  const [totalImages, setTotalImages] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     function getMovieImages() {
       getMovie({ query: 'IMAGES', filter: movieId })
         .then(data => {
           setMovieBackdrop(data.backdrops);
+          setTotalImages(data.backdrops.length);
+          setIsLoading(false);
         }) as Promise<TmdbMovieImageData>;
     }
 
@@ -26,40 +27,51 @@ export default function MovieBackdrop({ movieId, fallBack }: { movieId: string, 
     return () => { };
   }, [movieId])
 
+  const showNextImageButton = () => {
+    setCurrentImage(prevState => {
+      if (prevState === totalImages - 1) return 0;
+      return prevState + 1;
+    })
+  }
+
+  const showPrevImageButton = () => {
+    setCurrentImage(prevState => {
+      if (prevState === 0) return totalImages - 1;
+      return prevState - 1;
+    })
+  }
+
   return (
     <>
       {totalImages > 0 ? (
-        <div className="relative flex justify-center items-center w-full mt-2">
-          <Button
-            variant="outline"
-            size="sm"
-            className="absolute left-1 opacity-70 rounded-full"
-            disabled={currentImage === 0}
-            onClick={() => currentImage > 0 && setCurrentImage(prevState => prevState - 1)}
-          >{`<`}</Button>
-          {movieBackdrop.map((v, i) => {
-            const imgId = 'img-' + String(i);
-            return (
-              <img
-                key={imgId}
-                id={imgId}
-                className={twMerge(i === currentImage ? 'block' : 'hidden', 'rounded-md')}
-                src={`${import.meta.env.VITE_API_MOVIE_BACKDROP_URL}/${v.file_path}`}
-                alt="movie backdrop"
-                onError={() => (document.getElementById(imgId) as HTMLImageElement).src = ghostBackdrop}
-              />
-            )
-          })}
-          <Button
-            variant="outline"
-            size="sm"
-            className="absolute right-1 opacity-70 rounded-full"
-            disabled={currentImage === (totalImages - 1)}
-            onClick={() => setCurrentImage(prevState => prevState + 1)}
-          >{`>`}</Button>
+        <div className="relative w-full mt-2">
+          <div className='w-full h-full flex overflow-hidden'>
+            {movieBackdrop.map((v, i) => {
+              const imgId = 'img-' + String(i);
+              return (
+                <img
+                  key={imgId}
+                  id={imgId}
+                  className="block object-cover w-full h-full shrink-0 grow-0 ease-in-out duration-300 rounded-md"
+                  style={{ translate: `${-100 * currentImage}%` }}
+                  src={`${import.meta.env.VITE_API_MOVIE_BACKDROP_URL}/${v.file_path}`}
+                  alt="movie backdrop"
+                  onError={() => (document.getElementById(imgId) as HTMLImageElement).src = ghostBackdrop}
+                />
+              )
+            })}
+          </div>
+          <button
+            className="absolute block top-0 bottom-0 p-4 left-0 hover:bg-black/20 transition"
+            onClick={showPrevImageButton}
+          ><ChevronLeft className='h-8 w-8 text-white' /></button>
+          <button
+            className="absolute block top-0 bottom-0 p-4 right-0 hover:bg-black/20 transition"
+            onClick={showNextImageButton}
+          ><ChevronRight className='h-8 w-8 text-white' /></button>
         </div>
       ) : (
-        <img
+        !isLoading && <img
           id="movie-backdrop"
           className="w-full mt-2"
           src={`${import.meta.env.VITE_API_MOVIE_BACKDROP_URL}/${fallBack}`}
--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -11,7 +11,6 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 
 import { AppContext } from "@/context/AppProvider";
-// import ghostBackdrop from '@/assets/ghost-backdrop.jpg';
 import type { TmdbMovieDetailsData } from "@/lib/types";
 import MovieBackdrop from "@/components/MovieBackdrop";
 
@@ -21,7 +20,6 @@ export default function Movie({ routeId }: { routeId: number }) {
   const { appDispatch } = useContext(AppContext);
   const [activeTab, setActiveTab] = useState<ActiveTabProps>('info');
   const movieInfo = useLoaderData() as TmdbMovieDetailsData;
-  // const backdropUrl = import.meta.env.VITE_API_MOVIE_BACKDROP_URL;
 
   useEffect(() => {
     appDispatch({ type: 'SET_ROUTEID', payload: routeId });
@@ -33,14 +31,7 @@ export default function Movie({ routeId }: { routeId: number }) {
       <div className="max-w-[1024px] mx-auto my-2 p-2">
         <p className="text-orange-800 dark:text-orange-400 font-bold text-xl sm:text-3xl text-ellipsis overflow-hidden text-nowrap">{movieInfo.title}</p>
         <p className="text-gray-500 dark:text-gray-400">{movieInfo.tagline}</p>
-        {/* <img
-          id="movie-backdrop"
-          className="w-full mt-2"
-          src={`${backdropUrl}/${movieInfo.backdrop_path}`}
-          alt={movieInfo.title}
-          onError={() => (document.getElementById('movie-backdrop') as HTMLImageElement).src = ghostBackdrop}
-        /> */}
-        <MovieBackdrop movieId={String(movieInfo.id)} fallBack="movieInfo.backdrop_path" />
+        <MovieBackdrop movieId={String(movieInfo.id)} fallBack={movieInfo.backdrop_path} />
         <div className="mt-3 flex gap-3 justify-between flex-nowrap">
           <Button
             className="w-full"
