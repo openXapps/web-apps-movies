b462061d1c71dd98d4551a8f07101b03fae7f619
openXapps <32998735+openXapps@users.noreply.github.com>
2024-12-09 10:39:47 +0200

    Added more filtering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,7 +21,12 @@ export default function Home({ routeId }: { routeId: number }) {
     <div className="mt-2">
       {total_results > 0 ? (
         <div className="max-w-[1024px] mx-auto my-2 p-2 grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
-          {results.map(v => (v.poster_path && <MovieCard key={v.id} {...v} />))}
+          {results.map(v => (
+            !v.adult &&
+            v.vote_count > 4 &&
+            v.poster_path &&
+            <MovieCard key={v.id} {...v} />
+          ))}
         </div>
       ) : (
         <div className='text-center text-xl mt-10'>
