1cd4ddccdad2afafcb43621ab025d886584fc051
OpenApps <32998735+openXapps@users.noreply.github.com>
2025-03-16 14:27:22 +0200

    Fixed result filter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,8 @@ import { AppContext } from '@/context/AppProvider';
 import type { TmdbMovieList } from '@/lib/types';
 import { RouteId } from '@/lib/enums';
 
+const backInTime: number = Date.now() - (1000 * (60 * (60 * (24 * 30))));
+
 export default function Home({ routeId }: { routeId: number }) {
   const { total_results, total_pages, results } = useLoaderData() as TmdbMovieList;
   const { appDispatch } = useContext(AppContext);
@@ -22,12 +24,16 @@ export default function Home({ routeId }: { routeId: number }) {
     <div className="mt-2">
       {total_results > 0 ? (
         <div className="max-w-[1024px] mx-auto my-2 p-2 grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
-          {results.map(v => (
-            !v.adult &&
-            (v.vote_count > 4 || routeId === RouteId.UPCOMING) &&
-            v.poster_path &&
-            <MovieCard key={v.id} {...v} />
-          ))}
+          {results.map(v => {
+            const movieDate: number = new Date(v.release_date || '1970-01-01').getTime();
+            return (
+              !v.adult &&
+              // (v.vote_count > 4 || routeId === RouteId.UPCOMING) &&
+              v.poster_path &&
+              ((routeId === RouteId.NOW_PAYING && movieDate > backInTime) || true) &&
+              <MovieCard key={v.id} {...v} />
+            )
+          })}
         </div>
       ) : (
         <div className='text-center text-xl mt-10'>
